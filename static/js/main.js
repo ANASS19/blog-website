ea24d5d8e338446cb4f5d675c9c559c6631d74bf
ANASS19 <nassirri115@gmail.com>
2020-10-07 00:07:14 +0100

    push blog website flask python

---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var x = document.querySelectorAll('h2');
+    var number=x.length
+    document.getElementById("demo1").innerHTML = 'ANASS Web Development, Copyright &copy; 2020'+'   &nbsp;     Number Of Post Is '+ number ;
+}
+myFunction()
